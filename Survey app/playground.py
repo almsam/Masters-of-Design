909d44fe9f590c560f5b3bef18e6f6d5d3082e2f
almsam <44277999+almsam@users.noreply.github.com>
2026-07-02 15:06:48 -0700

    playground and run

---
+++ Survey app/playground.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+q2xa = 1
+q2xb = 5
+q2ya = 1
+q2yb = 5
+q3xa = 1
+q3xb = 5
+q3ya = 1
+q3yb = 5
+
+
+
+Q2x = ((q2xa - q2xb))*2.5
+
+Q2y = ((q2ya - q2yb))*2.5
+
+Q3x = ((q3xa - q3xb))*2.5
+
+Q3y = ((q3ya - q3yb))*2.5
+
+
+print(Q2x, Q2y, Q3x, Q3y)
