665e4384e6fadd7b1bb4bacbfee22568c65c4b53
almsam <44277999+almsam@users.noreply.github.com>
2026-07-03 14:43:03 -0700

    render deploy hotfix

--- Survey app/app.py
+++ Survey app/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 import matplotlib
 
@@ -201,4 +203,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
